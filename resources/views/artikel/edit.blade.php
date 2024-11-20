aa7737b2cc00205f2d503ccef909d1a434cfa044
DeveloperYuri <developeryuri2@gmail.com>
2024-11-20 13:40:29 +0700

    change dashboard layouts

--- resources/views/artikel/edit.blade.php
+++ resources/views/artikel/edit.blade.php
@@ -1,124 +1,57 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.masterdashboard')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create Artikel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<style>
-    .ck-editor__editable {
-        min-height: 300px;
-    }
-</style>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('images/logo-8.png')}}" width="100px"
-                    height="50px" /></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    
-                </ul>
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">Logout</a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
+@section('content')
     <div class="container">
 
         <h1 class="text-center mt-4 mb-4" style="color: #005EB8">Edit Artikel</h1>
 
         <a href="{{ route('artikel.index') }}" class="btn btn-success mb-3"><- Kembali </a>
 
-        <form action="{{ route('artikel.update', $artikels->id) }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
+                <form action="{{ route('artikel.update', $artikels->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
 
-            <div class="form-group mb-3">
-                <label class="font-weight-bold">IMAGE</label>
-                <input type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('title', $artikels->image) }}">
+                    <div class="form-group mb-3">
+                        <label class="font-weight-bold">IMAGE</label>
+                        <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
+                            value="{{ old('title', $artikels->image) }}">
 
-                <!-- error message untuk image -->
-                @error('image')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+                        <!-- error message untuk image -->
+                        @error('image')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                @enderror
-            </div>
 
-            <div class="form-group mb-3">
-                <label class="font-weight-bold">TITLE</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
-                value="{{ old('title', $artikels->title) }}" placeholder="Masukkan Judul Artikel">
+                    <div class="form-group mb-3">
+                        <label class="font-weight-bold">TITLE</label>
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                            value="{{ old('title', $artikels->title) }}" placeholder="Masukkan Judul Artikel">
 
-                <!-- error message untuk title -->
-                @error('title')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+                        <!-- error message untuk title -->
+                        @error('title')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                @enderror
-            </div>
 
-            <div class="form-group mb-3">
-                <label class="font-weight-bold">DESCRIPTION</label>
-                <textarea id="description" class="form-control @error('description') is-invalid @enderror" name="description"
-                    rows="5" placeholder="Masukkan Description Artikel">{{ old('description', $artikels->description) }}</textarea>
+                    <div class="form-group mb-3">
+                        <label class="font-weight-bold">DESCRIPTION</label>
+                        <textarea id="description" class="form-control @error('description') is-invalid @enderror" name="description"
+                            rows="5" placeholder="Masukkan Description Artikel">{{ old('description', $artikels->description) }}</textarea>
 
-                <!-- error message untuk description -->
-                @error('description')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+                        <!-- error message untuk description -->
+                        @error('description')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                @enderror
-            </div>
 
-            <button type="submit" class="btn btn-md btn-primary me-3">Update</button>
+                    <button type="submit" class="btn btn-md btn-primary me-3">Update</button>
 
-        </form>
+                </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/34.0.0/classic/ckeditor.js"></script>
-
-    <script>
-        // Initialize CKEditor
-        ClassicEditor
-            .create(document.querySelector('textarea'))
-
-            .then(editor => {
-                console.log('Editor was initialized', editor);
-            })
-            .catch(error => {
-                console.error('Error during initialization of the editor', error);
-            });
-    </script>
-
-</body>
-
-</html>
+@endsection
