120dce4071b62c5ab63ca79e49fb2e4a61a5cd2f
DeveloperYuri <developeryuri2@gmail.com>
2025-01-13 09:14:45 +0700

    update link logo to homepage

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -5,7 +5,7 @@
     <div class="menubar">
         <nav class="navbar navbar-expand-lg bg-body-white">
             <div class="container-fluid">
-                <img src="images/logo-8.png" width="100px" height="50px" />
+                <a href="{{ route('index') }}"><img src="images/logo-8.png" width="100px" height="50px" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
