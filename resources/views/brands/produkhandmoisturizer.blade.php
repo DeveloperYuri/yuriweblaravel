f53a21ef6070361efae8fdebc16804826eee1d9d
DeveloperYuri <developeryuri2@gmail.com>
2025-01-22 08:57:56 +0700

    update image brand handgel hand moisturizer

--- resources/views/brands/produkhandmoisturizer.blade.php
+++ resources/views/brands/produkhandmoisturizer.blade.php
@@ -2,36 +2,35 @@
 
 @section('head')
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Yuri - Hands Moisturizer</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link rel="icon" href="{{ asset('images/logo-8.png') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Yuri - Hands Moisturizer</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+        <link rel="icon" href="{{ asset('images/logo-8.png') }}" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Itim&family=Oswald:wght@200..700&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
-        rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Itim&family=Oswald:wght@200..700&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
+            rel="stylesheet">
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NKRW8V3SDC"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NKRW8V3SDC"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
 
-        gtag('config', 'G-NKRW8V3SDC');
-    </script>
-</head>
-
+            gtag('config', 'G-NKRW8V3SDC');
+        </script>
+    </head>
 @endsection
 
 @section('navbar')
@@ -40,8 +39,9 @@
         <nav class="navbar navbar-expand-lg bg-body-white">
             <div class="container-fluid">
                 <img src="images/logo-8.png" width="100px" height="50px" />
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -111,36 +111,26 @@
 
 @section('content')
     <div class="container">
-        <div class="yurihandmoisturizer-produk mt-4" data-aos="fade-up">
-            <div class="card mb-3 mx-auto" style="max-width: 900px;">
-                <div class="row g-0">
-                    <div class="col-lg-4">
-                        <img src="images/produkhandmoisturizer.png" class="img-fluid rounded-start">
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="card-body">
-                            <h5 class="card-title" style="color: #005EB8;">
-                                Yuri Hand Moisturizer </h5>
-                            <p class="card-text mt-3" style="text-align: justify;">
-                                - Sangat cocok untuk kulit normal dan sensitif (Label Dermatologically Test).
-                                <br>- Dapat membunuh kuman bakteri dalam 15 detik.
-                                <br>- Tidak mengandung alkohol.
-                                <br>- Tidak lengket dan Cepat kering.
-                                <br>- Lembut dan halus di tangan.
-                                <br>- Mudah di bawa-bawa.
-                            </p>
-                            <a href="https://www.yurishop.co/id/product/Yuri-Hand-Moisturizer-40-G"
-                                class="btn btn-primary fw-bold" role="button" target="_blank">Beli Sekarang</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="caption text-center mb-4">
+            <img src="images/infografisproduk/yuri/yurimoisturizer/logoyurimoisturizer.png" alt="" width="150px">
+        </div>
+
+        <div class="aganolgeneral">
+            <img src="images/infografisproduk/yuri/yurimoisturizer/yurimoisturizerinfograph.jpg" alt="" />
+        </div>
+
+        <div class="button-brand">
+            <a href="https://www.yurishop.co/id/product/Yuri-Hand-Moisturizer-40-G" target="_blank"><img
+                    src="images/bingkai klik-8.png" class="img-fluid" alt="Responsive image" width="30%"
+                    height="10%">
+                <div class="centered"><strong>Beli Sekarang</strong></div>
+            </a>
         </div>
     </div>
     <!-- End Container -->
 
     <!-- Start Footer -->
-    <div id="footerprodukaganol" class="card-footer text-body-secondary bg-dark text-center"
+    <div id="footerprodukhandgel" class="card-footer text-body-secondary bg-dark text-center"
         style="margin-top: 30px; height: 40px; padding-top: 5px;">
         <div style="margin-top: 5px;">
             <p style="font-size: 12px; color: white">Copyright © 2024 PT Joenoes Ikamulya.</p>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,37 +3,6 @@
 
 @yield('head')
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Yuri</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link rel="icon" href="{{ asset('images/logo-8.png') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Itim&family=Oswald:wght@200..700&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
-        rel="stylesheet">
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NKRW8V3SDC"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-NKRW8V3SDC');
-    </script>
-
-</head>
-
 <style>
     h1 {
         font-family: "Oswald", sans-serif;
@@ -50,7 +19,7 @@
             <!-- Sosmed -->
             <div class="sosmed mb-2">
                 <ul>
-                    <li><a href="kontak.html"><img src="{{ asset('images/kontak-small.png') }}" alt="kontak"></a></a>
+                    <li><a href="{{ route('index.kontak') }}"><img src="{{ asset('images/kontak-small.png') }}" alt="kontak"></a></a>
                     </li>
                     <li><a href="https://www.youtube.com/@YuriDee2" target="_blank"><img
                                 src="{{ asset('images/youtube-small.png') }}" alt="Youtube"></a></a></li>
