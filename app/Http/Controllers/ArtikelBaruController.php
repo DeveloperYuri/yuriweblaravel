a1936610ba135e94d9e44d6413e14579645067d4
DeveloperYuri <developeryuri2@gmail.com>
2025-01-30 11:26:06 +0700

    update 30 januari 2025

--- app/Http/Controllers/ArtikelBaruController.php
+++ app/Http/Controllers/ArtikelBaruController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ArtikelBaru;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 
 class ArtikelBaruController extends Controller
--- resources/views/artikelbaru/show.blade.php
+++ resources/views/artikelbaru/show.blade.php
@@ -2,46 +2,45 @@
 
 @section('head')
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="description"
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description"
             content="Yuri  berkomitmen untuk menghasilkan produk berkualitas internasional
 yang terjangkau oleh semua lapisan masyarakat">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Yuri</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link rel="icon" href="{{ asset('images/logo-8.png') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{{ $artikelbaru->title }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+        <link rel="icon" href="{{ asset('images/logo-8.png') }}" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Itim&family=Oswald:wght@200..700&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
-        rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Itim&family=Oswald:wght@200..700&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&display=swap"
+            rel="stylesheet">
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NKRW8V3SDC"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NKRW8V3SDC"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
 
-        gtag('config', 'G-NKRW8V3SDC');
-    </script>
-</head>
-
+            gtag('config', 'G-NKRW8V3SDC');
+        </script>
+    </head>
 @endsection
 
 @section('navbar')
     <div class="menubar">
         <nav class="navbar navbar-expand-lg bg-body-white">
             <div class="container-fluid">
-                <img src="{{ asset('images/logo-8.png') }}" width="100px" height="50px" alt="logo"/>
+                <img src="{{ asset('images/logo-8.png') }}" width="100px" height="50px" alt="logo" />
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -124,7 +123,11 @@ yang terjangkau oleh semua lapisan masyarakat">
         <div class="article1">
             <h4 class="mb-4 mt-3 text-center" style="color: #005EB8;">{{ $artikelbaru->title }}</h4>
         </div>
-        
+
+        <div class="daysarticle text-center" style="font-size: 12px">
+            <p><strong>Yuri Artikel</strong> - {{ $artikelbaru->created_at->isoFormat('dddd, D MMMM Y') }}</p>
+        </div>
+
         <!-- Banner Homepage-->
         <div class="banner-article1 text-center" data-aos="fade-up">
             <img src="{{ asset('/storage/artikelbaru/' . $artikelbaru->image) }}" class="img-fluid" alt="...">
@@ -139,5 +142,4 @@ yang terjangkau oleh semua lapisan masyarakat">
         </div>
 
     </div>
-
 @endsection
