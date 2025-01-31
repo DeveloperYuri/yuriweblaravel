50f94b2a3431a86eec2e88b499b769c92d4cac25
DeveloperYuri <developeryuri2@gmail.com>
2025-01-31 14:44:02 +0700

    update add brand infografis laudry disinfektant

--- resources/views/artikelbaru/index.blade.php
+++ resources/views/artikelbaru/index.blade.php
@@ -148,35 +148,78 @@
                 </h1>
             </div>
 
-            <div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
-                @forelse ($artikelbaru as $artikel)
-                    <div class="col-md-4">
-                        <div class="card h-100">
-                            <img src="{{ asset('/storage/artikelbaru/' . $artikel->image) }}" class="card-img-top"
-                                alt="..." width="400px" height="230px">
-                            <div class="card-body">
-                                <a href="{{ route('indexartikelbaru.show', $artikel->id)}}">
-                                    <h5 class="card-title" style="font-size: 20px;">{{ $artikel->title }}</h5>
-                                </a>
+            <!--
+                <div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
+                    @forelse ($artikelbaru as $artikel)
+    <div class="col-md-4">
+                            <div class="card h-100">
+                                <img src="{{ asset('/storage/artikelbaru/' . $artikel->image) }}"
+                                    class="card-img-top" alt="..." width="400px" height="170px">
+                                <div class="card-body">
+                                    <a href="{{ route('indexartikelbaru.show', $artikel->id) }}">
+                                        <h5 class="card-title" style="font-size: 15px;">{{ $artikel->title }}</h5>
+                                    </a>
+                                </div>
+
                             </div>
-
                         </div>
-                    </div>
                 @empty
-                    <div class="alert alert-danger">
-                        Data Products belum Tersedia.
+                        <div class="alert alert-danger">
+                            Data Products belum Tersedia.
+                        </div>
+    @endforelse
+
+                </div>
+
+                <div class="mt-5 mb-5">
+                    {!! $artikelbaru->withQueryString()->links('pagination::bootstrap-5') !!}
+                </div>
+            -->
+
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
+                        @forelse ($artikelbaru as $artikel)
+                            <div class="col-md-4">
+                                <div class="card h-100">
+                                    <img src="{{ asset('/storage/artikelbaru/' . $artikel->image) }}"
+                                        class="card-img-top" alt="..." width="400px" height="170px">
+                                    <div class="card-body">
+                                        <a href="{{ route('indexartikelbaru.show', $artikel->id) }}">
+                                            <h5 class="card-title" style="font-size: 15px;">{{ $artikel->title }}</h5>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        @empty
+                            <div class="alert alert-danger">
+                                Data Products belum Tersedia.
+                            </div>
+                        @endforelse
+
                     </div>
-                @endforelse
 
+                    <div class="mt-5 mb-5">
+                        {!! $artikelbaru->withQueryString()->links('pagination::bootstrap-5') !!}
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-3">
+                    <h5>Category</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item active" aria-current="true"><a href=""
+                                style="color: white; font-weight:bold">Pembersih Rumah Tangga</a></li>
+                        <li class="list-group-item"><a href=""
+                                style="color: #005EB8; font-weight:bold">Anak-Anak</a></li>
+                        <li class="list-group-item"><a href="" style="color: #005EB8; font-weight:bold">Bayi</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
-            <div class="mt-5 mb-5">
-                {!! $artikelbaru->withQueryString()->links('pagination::bootstrap-5') !!}
-            </div>
         </div>
         <!-- End Content1-->
 
     </div>
     <!-- End Container -->
-    
 @endsection
